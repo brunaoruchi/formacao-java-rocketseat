afc54346f35fc8c6d0706a900d441c928fb527cf
brunaoruchi <brunaquispeo@gmail.com>
2024-05-21 21:42:19 -0300

    Adicionando os Path Params no projeto

---
+++ primeiro-projeto-springboot/src/main/java/com/example/controller/controller/PrimeiraController.java
@@ -0,0 +1,25 @@
+package com.example.controller.controller;
+
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("/primeiraController")
+public class PrimeiraController {
+
+    @GetMapping("/primeiroMetodo/{id}")
+    public String primeiroMetodo(@PathVariable String id){
+        return "O parametro é: " + id;
+    }
+
+    @GetMapping("/metodoComQueryParams")
+    public String metodoComQueryParams(@RequestParam String id) {
+        return "O parametro com metodoComQueryParams é: " + id;
+    }
+
+    @GetMapping("/metodoComQueryParams2")
+    public String metodoComQueryParams2(@RequestParam Map<String, String> allParams) {
+        return "O parametro com metodoComQueryParams2 é: " + allParams.entrySet();
+    }
+}
