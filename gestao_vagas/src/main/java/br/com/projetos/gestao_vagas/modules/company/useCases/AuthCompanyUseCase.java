18f0999870281fb5dc459f7968c3ee17e3a1e651
brunaoruchi <brunaquispeo@gmail.com>
2024-06-18 20:22:31 -0300

    Finalização do UseCase AuthCompany

--- gestao_vagas/src/main/java/br/com/projetos/gestao_vagas/modules/company/useCases/AuthCompanyUseCase.java
+++ gestao_vagas/src/main/java/br/com/projetos/gestao_vagas/modules/company/useCases/AuthCompanyUseCase.java
@@ -2,7 +2,10 @@ package br.com.projetos.gestao_vagas.modules.company.useCases;
 
 import br.com.projetos.gestao_vagas.modules.company.dto.AuthCompanyDTO;
 import br.com.projetos.gestao_vagas.modules.company.repositories.CompanyRepository;
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -11,6 +14,8 @@ import javax.naming.AuthenticationException;
 
 @Service
 public class AuthCompanyUseCase {
+    @Value("${security.token.secret}")
+    private String secretKey;
 
     @Autowired
     private CompanyRepository companyRepository;
@@ -18,10 +23,10 @@ public class AuthCompanyUseCase {
     @Autowired
     private PasswordEncoder passwordEncoder;
 
-    public void execute(AuthCompanyDTO authCompanyDTO) throws AuthenticationException {
+    public String execute(AuthCompanyDTO authCompanyDTO) throws AuthenticationException {
         var company = this.companyRepository.findByUsername(authCompanyDTO.getUsername()).orElseThrow(
                 () -> {
-                    throw new UsernameNotFoundException("Company not found");
+                    throw new UsernameNotFoundException("Username/password incorrect");
                 }
         );
         //Verificar a senha está correta
@@ -29,9 +34,14 @@ public class AuthCompanyUseCase {
         var passwordMatches = this.passwordEncoder.matches(authCompanyDTO.getPassword(), company.getPassword());
 
         if (!passwordMatches) {
-            throw new AuthenticationException("Senha incorreta");
+            throw new AuthenticationException("Username/password incorrect");
         }
 
+        Algorithm algorithm = Algorithm.HMAC256(secretKey);
 
+        var token = JWT.create().withIssuer("javagas")
+                .withSubject(company.getId().toString()).sign(algorithm);
+
+        return token;
     }
 }
