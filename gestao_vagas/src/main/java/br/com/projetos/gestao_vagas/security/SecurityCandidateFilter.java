53a1548e238c51ceff1012df5e57eba41f685f11
brunaoruchi <brunaquispeo@gmail.com>
2025-01-22 20:28:45 -0300

    Add configuração do PreAuthorize

--- gestao_vagas/src/main/java/br/com/projetos/gestao_vagas/security/SecurityCandidateFilter.java
+++ gestao_vagas/src/main/java/br/com/projetos/gestao_vagas/security/SecurityCandidateFilter.java
@@ -6,6 +6,8 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
@@ -22,7 +24,7 @@ public class SecurityCandidateFilter extends OncePerRequestFilter {
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
             throws ServletException, IOException {
 
-        SecurityContextHolder.getContext().setAuthentication(null);
+//        SecurityContextHolder.getContext().setAuthentication(null);
         String header = request.getHeader("Authorization");
 
         if(request.getRequestURI().startsWith("/candidate")){
@@ -32,8 +34,16 @@ public class SecurityCandidateFilter extends OncePerRequestFilter {
                     response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
                 }
                 request.setAttribute("candidate_id", token.getSubject());
-                System.out.println("======= TOKEN ============");
-                System.out.println(token.getClaim("roles"));
+                var roles = token.getClaim("roles").asList(Object.class);
+
+                var grants = roles.stream().map(
+                        role -> new SimpleGrantedAuthority("ROLE_" + role.toString().toUpperCase())
+                ).toList();
+
+                UsernamePasswordAuthenticationToken auth = new UsernamePasswordAuthenticationToken(token.getSubject()
+                        ,null,grants);
+                
+                SecurityContextHolder.getContext().setAuthentication(auth);
             }
         }
 
