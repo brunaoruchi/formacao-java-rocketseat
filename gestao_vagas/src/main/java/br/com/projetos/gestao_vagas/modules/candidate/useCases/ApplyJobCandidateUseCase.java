8d6913809c41ffaeb6137e4007c013cd0845d6df
brunaoruchi <brunaquispeo@gmail.com>
2025-07-01 22:43:51 -0300

    Criando entidade de Apply Job

--- gestao_vagas/src/main/java/br/com/projetos/gestao_vagas/modules/candidate/useCases/ApplyJobCandidateUseCase.java
+++ gestao_vagas/src/main/java/br/com/projetos/gestao_vagas/modules/candidate/useCases/ApplyJobCandidateUseCase.java
@@ -3,6 +3,7 @@ package br.com.projetos.gestao_vagas.modules.candidate.useCases;
 import br.com.projetos.gestao_vagas.exceptions.JobNotFoundException;
 import br.com.projetos.gestao_vagas.exceptions.UserNotFoundException;
 import br.com.projetos.gestao_vagas.modules.candidate.CandidateRepository;
+import br.com.projetos.gestao_vagas.modules.candidate.repository.ApplyJobRepository;
 import br.com.projetos.gestao_vagas.modules.company.repositories.JobRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -17,6 +18,9 @@ public class ApplyJobCandidateUseCase {
 
     @Autowired
     private JobRepository jobRepository;
+
+    @Autowired
+    private ApplyJobRepository applyJobRepository;
 
     //ID do candidato && ID da vaga
     public void execute(UUID idCandidate, UUID idJob) {
